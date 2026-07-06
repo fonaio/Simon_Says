d7d74aff3076ffe9d2bf45a85ab1d77a89c65d06
fonaio <flin3004825@gmail.com>
2026-07-06 14:53:34 -0500

    created game control

---
+++ Simon_Says.srcs/sources_1/new/Game_Control.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/01/2026 06:27:16 PM
+// Design Name: 
+// Module Name: Game_Control
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Game_Control(
+
+    );
+endmodule
