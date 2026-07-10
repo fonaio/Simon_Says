70a4fffca5674faf10d9542df0e55751840fb99c
fonaio <flin3004825@gmail.com>
2026-07-10 13:43:35 -0500

    finished pregame module

---
+++ Pregame.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/10/2026 05:56:43 PM
+// Design Name: 
+// Module Name: Pregame
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Pregame(
+
+    );
+endmodule
