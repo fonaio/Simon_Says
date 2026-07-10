f5dc9339a481a3af44fd5735d60e2fbe3551fef2
fonaio <flin3004825@gmail.com>
2026-07-10 15:54:29 -0500

    created clock divider

---
+++ clk_divider.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/10/2026 08:23:30 PM
+// Design Name: 
+// Module Name: clk_divider
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clk_divider(
+
+    );
+endmodule
