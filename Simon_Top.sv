17547ad261ad20c9010bfe9b0579dcd2efe53915
fonaio <flin3004825@gmail.com>
2026-07-15 13:51:08 -0500

    Updated Simon Top module with pregame and game control

--- Simon_Top.sv
+++ Simon_Top.sv
@@ -21,6 +21,44 @@
 
 
 module Simon_Top(
-
+    input logic clk,
+    input logic reset,
+    input logic BTNC,
+    
+    output led0_top, //LD 17
+    output led1_top //LD 16
     );
+    
+    logic [2:0] game_state;
+    logic pregame_start;
+    logic [2:0] pregame_led0;
+    logic [2:0] pregame_led1;
+    
+    Game_Control u_game_control (
+    .clk(clk),
+    .reset(reset),
+    .start(pregame_start),
+    .
+    
+    )
+    
+    Pregame u_pregame (
+    .start_button(BTNC)
+    .start(pregame_start)
+    .led0(pregame_led0)
+    .led1(pregame_led1)
+    )
+    
+    always_comb begin
+        if (game_state == 3'b000) begin
+            led0_top = pregame_led0;
+            led1_top = pregame_led1;
+        end
+        else begin
+            led0_top = 3'b000;
+            led1_top = 3'b000;
+        end
+    end
+    
+    module Pregame 
 endmodule
