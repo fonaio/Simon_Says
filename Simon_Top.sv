a5db7ed8c3cae812f9ab2440d0474937454d8ae4
fonaio <flin3004825@gmail.com>
2026-07-14 21:14:00 -0500

    fixed game controls to check after each button press, added top logic module, and simplified pregame LED code

---
+++ Simon_Top.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/15/2026 01:43:20 AM
+// Design Name: 
+// Module Name: Simon_Top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Simon_Top(
+
+    );
+endmodule
