961e2edc40374afd1b4cdd8b216099062450416a
fonaio <flin3004825@gmail.com>
2026-07-15 14:35:29 -0500

    completed user_pressing module

---
+++ User_Pressing.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/15/2026 06:51:43 PM
+// Design Name: 
+// Module Name: User_Pressing
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module User_Pressing(
+
+    );
+endmodule
