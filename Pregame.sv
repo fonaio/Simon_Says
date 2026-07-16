11b4459f4048cd50d3e799a4c29523aa7f9285ca
fonaio <flin3004825@gmail.com>
2026-07-16 17:09:21 -0500

    added pregame states for array filling and added new variables to combine simon_brain with pregame

--- Pregame.sv
+++ Pregame.sv
@@ -6,14 +6,19 @@
 
 // Module Name: Pregame
 // Project Name: Simon_Says
-// Description: Sets 2 Tricolor LEDs to be green until the game starts. After the game starts, LEDs turn off.
+// Description: Sets 2 Tricolor LEDs to be green after filling memory array. Pressing the button when the LED is green starts the game. 
+// After the game starts, LEDs turn off.
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module Pregame(
-
+    input logic clk,
+    input logic reset,
     input logic start_button,
-
+    input logic [1:0] rand_value, //brain
+    
+    output logic [4:0] memindex, //brain
+    output logic is_writing,
     output logic start,
     output logic [2:0] led0, // {rgb}
     output logic [2:0] led1 
